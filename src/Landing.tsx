0614296e33bdba5c4259fc8717d964ae5dc7b994
Fernando Gómez Graciani <fernando.gomez@arangodb.com>
2024-07-30 00:44:48 +0200

    render spline after hydration

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -7,7 +7,12 @@ import * as icons from "simple-icons";
 import data from "../data.json";
 import { getDescendantProp } from "./util";
 import Spline from "@splinetool/react-spline";
+import dynamic from "next/dynamic";
 const { landing } = data;
+
+const DynamicSpline = dynamic(() => import("./Spline"), {
+  ssr: false,
+});
 
 const SplineWrapper = styled(Box)({
   position: "absolute",
@@ -96,7 +101,7 @@ export default function Landing() {
           <>
             <Grid item lg={6}></Grid>
             <SplineWrapper>
-              <Spline scene="https://prod.spline.design/MXlM9tgFBK6WdzPl/scene.splinecode" />;
+              <DynamicSpline scene="https://prod.spline.design/MXlM9tgFBK6WdzPl/scene.splinecode" />;
             </SplineWrapper>
             {/* <Image src="/landing.svg" alt="Landing" width="616" height="787" /> */}
           </>
