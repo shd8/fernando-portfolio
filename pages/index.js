a4c39205eb0293acac0dfc94f74190276c66b021
Fernando Gómez Graciani <fernando.gomez@arangodb.com>
2024-07-29 16:20:43 +0200

    feat: include analytics

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import About from "../src/About";
 import data from "../data.json";
 import { darkTheme, lightTheme } from "../src/theme";
 import { Brightness4, Brightness7 } from "@material-ui/icons";
+import { Analytics } from "@vercel/analytics/react";
 const { name, projects } = data;
 
 const useStyles = makeStyles((theme) => ({
@@ -60,6 +61,7 @@ export default function Index({ projects, setTheme }) {
 
   return (
     <div className={classes.root}>
+      <Analytics />
       <AppBar color={!trigger ? "transparent" : "inherit"} className={classes.appBar} position="fixed">
         <Toolbar>
           <Typography variant="h6" className={classes.root}>
