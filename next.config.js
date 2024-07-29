893821cc745e67b7b9ef451ddc65565997980d99
Fernando Gómez Graciani <fernando.gomez@arangodb.com>
2024-07-29 19:01:39 +0200

    Revert "fix: remove pwa"
    This reverts commit 81e7cae8c74e7f3ccd4959d3a84127da6d613a2c.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-// @ts-check
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
-
-module.exports = nextConfig;
+module.exports = withPWA({
+    pwa: {
+        disable: process.env.NODE_ENV === 'development',
+        dest: 'public'
+    },
+    images: {
+        domains: ['media-exp1.licdn.com', 'www.nonceblox.com']
+    }
+})
